8215da452eb3a52202dd78cc98680e81b61bbab1
Yoleny <yoleny.salme28@gmail.com>
2023-10-30 16:43:24 -0600

    se agrego la clase invertebrados y se termino de agregar los nombres de los 4 animales.

---
+++ Ejercicio Herncia/Invertebrado.cs
@@ -0,0 +1,31 @@
+﻿using Ejercicio_Herencia;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_Herncia
+{
+    internal class Invertebrado : Animal
+    {
+
+
+
+        public string? tipo;
+        public bool tienePatas;
+        public string? numeroPatas;
+        public bool tieneConcha;
+       
+
+        public void imprimirInvertebrado()
+
+
+        {
+            Console.WriteLine("Tipo: " + tipo);
+            Console.WriteLine("Tiene Patas: " + tienePatas);
+            Console.WriteLine("numero de Patas: " + numeroPatas);
+            Console.WriteLine("Tiene Concha: " + tieneConcha);
+        }
+    }
+}
--- Ejercicio Herncia/Program.cs
+++ Ejercicio Herncia/Program.cs
@@ -1,15 +1,88 @@
 ﻿using Ejercicio_Herencia;
+using Ejercicio_Herncia;
 
-Animal animal = new Animal();
-animal.nombre = "Perro";
-animal.color = "Blanco";
-animal.tamano = "Mediano";
-animal.familia = "Husky";
+Vertebrado vertebrado = new Vertebrado();
+vertebrado.nombre = "Perro";
+vertebrado.color = "Blanco";
+vertebrado.tamano = "Mediano";
+vertebrado.familia = "Husky";
+vertebrado.tipo = "Mamifero";
+vertebrado.tienePatas = true;
+vertebrado.esDomestico = true;
+vertebrado.esHervivoro = false;
+vertebrado.esCarnivoro = true;
 
 
-if (animal.validar())
+if (vertebrado.validar())
 {
-    animal.imprimir();
+    vertebrado.imprimir();
+    vertebrado.imprimirVertebrado();
+}
+else
+{
+    Console.WriteLine("ingrese todos los datos");
+}
+
+
+vertebrado.nombre = "Oso";
+vertebrado.color = "Negro";
+vertebrado.tamano = "Grande";
+vertebrado.familia = "Salvaje";
+vertebrado.tipo = "Mamifero";
+vertebrado.tienePatas = true;
+vertebrado.esDomestico = false;
+vertebrado.esHervivoro = false;
+vertebrado.esCarnivoro = true;
+
+
+if (vertebrado.validar())
+{
+    vertebrado.imprimir();
+    vertebrado.imprimirVertebrado();
+}
+else
+{
+    Console.WriteLine("ingrese todos los datos");
+}
+
+
+Invertebrado invertebrado = new Invertebrado();
+
+invertebrado.nombre = "Tarantula";
+invertebrado.color = "Negra";
+invertebrado.tamano = "Pequeno";
+invertebrado.familia = "Theraphosidae";
+invertebrado.tipo = "Aracnido";
+invertebrado.tienePatas = true;
+invertebrado.numeroPatas = "8";
+invertebrado.tieneConcha = false;
+
+
+if (invertebrado.validar())
+{
+    invertebrado.imprimir();
+    invertebrado.imprimirInvertebrado();
+}
+else
+{
+    Console.WriteLine("ingrese todos los datos");
+}
+
+
+invertebrado.nombre = "Caracol";
+invertebrado.color = "Blanco";
+invertebrado.tamano = "Pequeno";
+invertebrado.familia = "Helicidios";
+invertebrado.tipo = "Hekix pomatia";
+invertebrado.tienePatas = false;
+invertebrado.numeroPatas = "no tiene patas";
+invertebrado.tieneConcha = true;
+
+
+if (invertebrado.validar())
+{
+    invertebrado.imprimir();
+    invertebrado.imprimirInvertebrado();
 }
 else
 {
