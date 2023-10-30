b073ac312767c958974ce9e4872f3679c1a2b3f7
Yoleny <yoleny.salme28@gmail.com>
2023-10-30 15:31:43 -0600

    se agrego la clase padre Animal

---
+++ Ejercicio Herncia/Animal.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_Herencia
+{
+     public class Animal
+    {
+        public string? nombre;
+        public string? color;
+        public string? tamano;
+        public string? familia;
+
+        /* Metodo para imprimir datos*/
+
+        public void imprimir()
+
+        {
+            if (validar())
+            {
+                Console.WriteLine("Nombre: " + nombre);
+                Console.WriteLine("Nombre: " + color);
+                Console.WriteLine("Nombre: " + tamano);
+                Console.WriteLine("Nombre: " + familia);
+            }
+        }
+
+
+        public bool validar()
+        {
+            if (nombre != null && color != null && tamano != null && familia != null)
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
