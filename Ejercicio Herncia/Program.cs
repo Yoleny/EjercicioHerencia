7693be0d40270c1e62c8532c0ef3b624bba03eaa
Yoleny <yoleny.salme28@gmail.com>
2023-10-30 16:00:04 -0600

    se agrego clase Vertebrado

--- Ejercicio Herncia/Program.cs
+++ Ejercicio Herncia/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Ejercicio_Herencia;
+
+Animal animal = new Animal();
+animal.nombre = "Perro";
+animal.color = "Blanco";
+animal.tamano = "Mediano";
+animal.familia = "Husky";
+
+
+if (animal.validar())
+{
+    animal.imprimir();
+}
+else
+{
+    Console.WriteLine("ingrese todos los datos");
+}
---
+++ Ejercicio Herncia/Vertebrado.cs
@@ -0,0 +1,36 @@
+﻿using Ejercicio_Herencia;
+using Ejercicio_Herencia;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Ejercicio_Herencia
+{
+    internal class Vertebrado : Animal
+    {
+
+
+        public string? tipo;
+        public bool tienePatas;
+        public bool esDomestico;
+        public bool esHervivoro;
+        public bool esCarnivoro;
+
+        public void imprimirVertebrado()
+
+
+        {
+                Console.WriteLine("Tipo: " + tipo);
+                Console.WriteLine("Tiene Patas: " + tienePatas);
+                Console.WriteLine("Es Domestico: " + esDomestico);
+                Console.WriteLine("Es Hervivoro: " + esHervivoro);
+                Console.WriteLine("Es Carnivoro: " + esCarnivoro);
+            
+        }
+
+    }
+}
